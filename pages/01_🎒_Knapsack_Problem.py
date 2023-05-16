ae88e2c8200574928fa84a20f1019e1ad407549e
Helder <helder.lima@easyc.com.br>
2023-05-16 11:25:30 -0300

    fix: rafaela cunha

--- pages/01_🎒_Knapsack_Problem.py
+++ pages/01_🎒_Knapsack_Problem.py
@@ -1,11 +1,11 @@
 import random
-
+import time
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import Home
 
 st.image("images/META_logo.png", width=150)
+
 st.markdown("""
 
 # Knapsack Problem
@@ -16,58 +16,96 @@ Imagine que você tenha uma mochila com capacidade limitada de peso e um conjunt
 
 Dado um conjunto de n itens, cada um com peso p_i e valor v_i, e uma mochila com capacidade máxima W, nosso objetivo é encontrar uma solução x = (x_1, x_2, ..., x_n) que maximize a função objetivo:
 
-$$f(x) = \sum_{i=1}^{n} v_i x_i$$
 """)
 
 st.markdown("## Escolha o algoritmo para resolver o problema:")
 
 algorithms = ["Algoritmo Genético", "Algoritmo de Enxame de Partículas", "Otimização de Colônia de Formigas", "Simulated Annealing"]
 selected_algorithm = st.selectbox("Algoritmo", algorithms)
-st.markdown("Faça o upload do seu arquivo abaixo:")
 
-uploaded_file = st.file_uploader("Selecione o arquivo", type=['txt', 'csv', 'json', 'xlsx'])
-if uploaded_file is not None:
-    file_details = {
-        "Filename": uploaded_file.name,
-        "FileType": uploaded_file.type,
-        "FileSize": uploaded_file.size
-    }
-    st.write(file_details)
-
-    # Exibir o conteúdo do arquivo (assumindo que é um arquivo de texto)
-    st.text(uploaded_file.getvalue())
-
-if selected_algorithm == "Algoritmo de Enxame de Partículas":
-    code = """
-# Input
-PARAMETERS = {'PERCENTAGE STD (SIGMA)': 10.0} # equal 10%
-
-SETUP = {
-        'N_REP': 10,
-        'N_POP': 1,
-        'N_ITER': 100,
-        'X_L': [-2, -2, -2],
-        'X_U': [2, 2, 2],
-        'D': 3,
-        'NULL_DIC': None,
-        'PARAMETERS': PARAMETERS
-        }
-
-# OF statement
-def OF_FUNCTION(X, NULL_DIC):
-    X_0 = X[0]
-    X_1 = X[1]
-    X_2 = X[2]
-    OF = X_0 ** 2 + X_1 ** 2 + X_2 ** 2
-    return OF
-
-# Call algorithm
-RESULTS_REP, BEST_REP, AVERAGE_REP, WORST_REP, STATUS_PROCEDURE = HILL_CLIMBING_001(OF_FUNCTION, SETUP)
+if selected_algorithm == "Algoritmo Genético":
+    st.markdown(
     """
-    st.code(code, language="python")
-    if st.button("Run"):
-        random_number = random.randint(1, 100)
-        st.write(f"Número gerado: {random_number}")
+    # Algoritmo Genético
+    """
+    )
+
+elif selected_algorithm == "Algoritmo de Enxame de Partículas":
+    st.markdown(
+    """
+    # Algoritmo de Enxame de Partículas
+    Particle Swarm Optimization:
+     O funcionamento se dá pela evolução de um enxame aleatório de indivíduos dispostos em um espaço d-dimensional, que a cada iteração, são atualizados de acordo com a melhor Função Objetivo (OF) individual valor e grupo. Assim, o algoritmo PSO é estruturado em dois princípios para o movimento de partículas. A primeira refere-se à influência do grupo sobre cada indivíduo e a segunda cria um tipo de memória em cada indivíduo do enxame.
+    """
+    )
+    st.markdown(
+    """
+    ## Setup
+    O funcionamento se dá pela evolução de um enxame aleatório de indivíduos dispostos em um espaço d-dimensional, que a cada iteração, são atualizados de acordo com a melhor Função Objetivo (OF) individual valor e grupo. Assim, o algoritmo PSO é estruturado em dois princípios para o movimento de partículas. A primeira refere-se à influência do grupo sobre cada indivíduo e a segunda cria um tipo de memória em cada indivíduo do enxame.
+    """
+    )
+    N_REP = st.number_input("N_REP", format="%0.0f")
+    N_POP = st.number_input("N_POP", format="%0.0f")
+    N_ITER = st.number_input("N_ITER", format="%0.0f")
+
+    instances = ["Instance 1", "Instance 2", "Instance 3", "Instance 4", "Instance 5", "Custom"]
+    selected_instance = st.selectbox("Instance", instances)
+    if selected_instance == "Instance 1":
+        st.markdown(
+        """
+            | Nome da Instância | Número de Objetos | Capacidade (c_max) | Ótimo    |
+            |-------------------|-------------------|--------------------|----------|
+            | F1-LOWKP-10D      | 10                | 269                | 29 5     |
+            | F2-LOWKP-20D      | 20                | 878                | 1024     |
+            | F3-LOWKP-4D       | 4                 | 20                 | 35       |
+            | F4-LOWKP-4D       | 4                 | 11                 | 23       |
+            | F5-LOWKP-15D      | 15                | 375                | 48,10694 |
+            | F6-LOWKP-10D      | 10                | 60                 | 52       |
+            | F7-LOWKP-7D       | 7                 | 50                 | 107      |
+            | F8-LOWKP-23D      | 23                | 10000              | 9767     |
+            | F9-LOWKP-5D       | 5                 | 80                 | 130      |
+            | F10-LOWKP-20D     | 20                | 879                | 1025     |
+        """
+        )
+    elif selected_instance == "Custom":
+        st.markdown("Faça o upload do seu arquivo abaixo:")
+        uploaded_file = st.file_uploader("Selecione o arquivo", type=['txt', 'csv', 'json', 'xlsx'])
+        if uploaded_file is not None:
+            file_details = {
+                "Filename": uploaded_file.name,
+                "FileType": uploaded_file.type,
+                "FileSize": uploaded_file.size
+            }
+            st.write(file_details)
+
+            # Exibir o conteúdo do arquivo (assumindo que é um arquivo de texto)
+            st.text(uploaded_file.getvalue())
+    if st.button ("Play"):
+        st.markdown(
+        """
+        Run algorithm
+        """
+        )
+    else:
+        pass
+    st.markdown(
+    """
+    ## Report results
+    ### etc...
+    """
+    )
+elif selected_algorithm == "Otimização de Colônia de Formigas":
+    st.markdown(
+    """
+    # Otimização de Colônia de Formigas
+    """
+    )
+elif selected_algorithm == "Simulated Annealing":
+    st.markdown(
+    """
+    # Simulated Annealing
+    """
+    )
 
 
 
@@ -86,49 +124,16 @@ RESULTS_REP, BEST_REP, AVERAGE_REP, WORST_REP, STATUS_PROCEDURE = HILL_CLIMBING_
 
 
 
-
-
-
-
-
-
-# st.header("Exemplo Hill Climbing")
-# code = """
-# # import numpy as np
-# from META_TOOLBOX import HILL_CLIMBING_001 # or from META_TOOLBOX import *
+# st.markdown("Faça o upload do seu arquivo abaixo:")
+# uploaded_file = st.file_uploader("Selecione o arquivo", type=['txt', 'csv', 'json', 'xlsx'])
+# if uploaded_file is not None:
+#     file_details = {
+#         "Filename": uploaded_file.name,
+#         "FileType": uploaded_file.type,
+#         "FileSize": uploaded_file.size
+#     }
+#     st.write(file_details)
 #
-# # Input
-# PARAMETERS = {'PERCENTAGE STD (SIGMA)': 10.0} # equal 10%
-#
-# SETUP = {
-#         'N_REP': 10,
-#         'N_POP': 1,
-#         'N_ITER': 100,
-#         'X_L': [-2, -2, -2],
-#         'X_U': [2, 2, 2],
-#         'D': 3,
-#         'NULL_DIC': None,
-#         'PARAMETERS': PARAMETERS
-#         }
-#
-# # OF statement
-# def OF_FUNCTION(X, NULL_DIC):
-#     X_0 = X[0]
-#     X_1 = X[1]
-#     X_2 = X[2]
-#     OF = X_0 ** 2 + X_1 ** 2 + X_2 ** 2
-#     return OF
-#
-# # Call algorithm
-# RESULTS_REP, BEST_REP, AVERAGE_REP, WORST_REP, STATUS_PROCEDURE = HILL_CLIMBING_001(OF_FUNCTION, SETUP)
-# """
-# st.code(code, language='python')
-#
-# # Carrega o arquivo XLSX em um DataFrame do Pandas
-# arquivo_xlsx = "META_HC001_RESUME_20230412 015821.xlsx"
-# dados = pd.read_excel(arquivo_xlsx)
-#
-# # Exibe a tabela na página
-# st.dataframe(dados)
-
+#     # Exibir o conteúdo do arquivo (assumindo que é um arquivo de texto)
+#     st.text(uploaded_file.getvalue())
 
