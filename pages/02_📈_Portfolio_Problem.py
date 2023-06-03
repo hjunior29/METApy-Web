a60406d01ebbfef74b88dc5d6dbdd1a9c383a5dc
Rafaela-SCunha <rafaela.cunha1972@gmail.com>
2023-06-02 23:18:31 -0300

    Portfolio

--- pages/02_📈_Portfolio_Problem.py
+++ pages/02_📈_Portfolio_Problem.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import date
 import datetime
-import streamlit.components.v1 as components
+import FA_PORTIFOLIO
 
 st.image("images/META_logo.png", width=150)
 st.title("Portfolio Problem")
@@ -15,7 +15,7 @@ st.write("""
 
 st.write("""
     <div style='text-align: justify'>
-A implementação da uma otimização de portfólio é feita utilizando o algoritmo Firefly. O objetivo é encontrar a alocação ótima de um portfólio, que maximize o índice de Sharpe, que relaciona o retorno do portfólio com o seu risco, com a introdução de restrições de limites de investimento em cada ativo do portfólio.
+A implementação da otimização de portfólio é feita utilizando o algoritmo Firefly. O objetivo é encontrar a alocação ótima de um portfólio, que maximize o índice de Sharpe, que relaciona o retorno do portfólio com o seu risco, com a restrição de que o somatório do conjunto de ativos do portfólio tem que ser igual a 100%.
 A implementação está estruturada da seguinte forma:
 </div>
 """, unsafe_allow_html=True)
@@ -29,8 +29,8 @@ st.write("""
  </div>
 """, unsafe_allow_html=True)
 
-equacao_ret = "**Return = ['Close'][J + 1] $\div$ ['Close'][J] - 1**"
-st.markdown(equacao_ret, unsafe_allow_html=True)
+
+st.latex("""Return = {['Close'][J + 1]}{['Close'][J] - 1}""")
 
 st.write("""
 Onde:
@@ -39,8 +39,8 @@ Onde:
 
 ['Close'][J] - 1: preço de fechamento do dia anterior.
 """)
-equacao_cov = "**COVARIANCE = RETURN.cov()**"
-st.write(equacao_cov, unsafe_allow_html=True)
+
+st.latex("""COVARIANCE = RETURN.cov() """)
 
 st.write("""
     <div style='text-align: justify'>
@@ -50,8 +50,7 @@ st.write("""
 </div>
 """, unsafe_allow_html=True)
 
-eq_pot_ret = "**Retorno <sub>carteira</sub> = $\sum$ X[I] * RETURN[I] * T_Y**"
-st.write(eq_pot_ret, unsafe_allow_html=True)
+st.latex("""Retorno_{carteira} = \sum X[I] * RETURN[I] * T_Y""")
 
 st.write("""
 Onde:
@@ -63,9 +62,7 @@ RETURN: é a lista de retorno dos ativos.
 T_Y: é o número de dias que a bolsa  opera em 1 ano.
 """)
 
-eq_pot_var = "***VAR = $\sum$ X[I] * X[J] * COVARIANCE[I, J] *T_Y***"
-
-st.write(eq_pot_var, unsafe_allow_html=True)
+st.latex("""VAR = \sum X[I] * X[J] * COVARIANCE[I, J] *T_Y """)
 
 st.write("""
 Onde: 
@@ -77,8 +74,7 @@ COVARIANCE[I,J]: é a matriz de covariância.
 T_Y: é o número de dias que a bolsa  opera em 1 ano.
 """)
 
-eq_risco = "**Risco<sub>carteira</sub> = $\sqrt{Var}$**"
-st.write(eq_risco, unsafe_allow_html=True)
+st.latex("""Risco_{carteira} = \sqrt{Var}""")
 
 
 st.write("""
@@ -104,15 +100,30 @@ selected_Optimization = st.selectbox("**Optimization:**", Optimization)
 if selected_Optimization == "Firefly Algorithm":
 
     #seleciona o conjuntos de ativo
-    stocks_set = ["Selecione o conjunto de ativos","Conjunto de ativos 1", "Conjunto de ativos 2", "Conjunto de ativos 3", "Custom"]
+    stocks_set = ["Selecione o conjunto de ativos","USA-1", "USA-2", "USA-3", "USA-4"]
     selected_instance = st.selectbox("**Conjunto de Ativos**", stocks_set)
     
-    if selected_instance == "Conjunto de ativos 1":
+    if selected_instance == "USA-1":
         st.markdown(
         """
-        | Empresa                 | Símbolo   |
-        |-------------------------|-----------|
-        |WEG S.A.                 | WEGE3.SA  |
+        | Empresa         | Símbolo   | Data Inicial | Data Final |
+        |-----------------|-----------|--------------|------------|
+        |Apple            | AAPL      | 01/01/2016   | 31/12/2017 |
+        |Amazon           | AMZN      | 
+        |Meta Platforms   | META      | 
+        |Google           | GOOGL     |
+        
+        """
+        )
+        if st.button ("**Play**"):
+            FA_PORTIFOLIO.FIREFLY()
+    
+    elif selected_instance == "USA-2":
+        st.markdown(
+        """
+        | Empresa                 | Símbolo   | Data Inicial | Data Final |
+        |-------------------------|-----------|--------------|------------|
+        |WEG S.A.                 | WEGE3.SA  | 01/01/2016   | 31/12/201  |
         |Lojas Renner S.A.        | LREN3.SA  | 
         |Vale S.A.                | VALE3.SA  | 
         |Petróleo Brasileiro S.A. | PETR4.SA  |
@@ -120,238 +131,110 @@ if selected_Optimization == "Firefly Algorithm":
    
         """
         )
-        stocks_set_1 = ["WEGE3.SA", "LREN3.SA", "VALE3.SA", "PETR4.SA", "EQTL3.SA"]
+        stocks_set_2 = ["WEGE3.SA", "LREN3.SA", "VALE3.SA", "PETR4.SA", "EQTL3.SA"]
 
-        start_day = datetime.date.today()
-        start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
-        end_day = date.today()
-        end_day = st.date_input("Selecione a data final",max_value=end_day)
+        #start_day = datetime.date.today()
+        #start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
+        #end_day = date.today()
+        #end_day = st.date_input("Selecione a data final",max_value=end_day)
         
-        if end_day < start_day:
-            st.error("A data final deve ser posterior à data inicial.")
+        #if end_day < start_day:
+         #   st.error("A data final deve ser posterior à data inicial.")
         
 #Setup e parametros
-        st.markdown("**Design space**")
-        st.markdown(
-        """
-        X_L = [0.00001] * D
-
-        X_U = [0.99999] * D
-        """)
-        st.markdown("**Setup FA method**")
-        st.markdown(
-        """" 
-        
-        GAMMA = GAMMA_ASSEMBLY(X_L, X_U, D, 2)
-        PARAMETERS = {
-                   'ATTRACTIVENESS (BETA_0)': 0.98,
-                   'MIN. RANDOM FACTOR (ALPHA_MIN)': 0.20,
-                   'MAX. RANDOM FACTOR (ALPHA_MAX)': 0.95,
-                   'LIGHT ABSORPTION (GAMMA)': GAMMA,
-                   'THETA': 0.98,
-                   'TYPE ALPHA UPDATE': 'YANG 0',
-                    'SCALING (S_D)': True
-        }""")
-        
-        st.markdown("**Setup optimization**")
-        st.markdown("{")
-        N_REP = st.number_input("N_REP", format="%0.0f")
-        N_POP = st.number_input("N_ITER", format="%0.0f")
-        N_ITER = st.number_input("N_POP", format="%0.0f")
-        st.markdown(
-        """
-        'D':   D,                                                                                                 
-        'X_L': X_L,                                                                                              
-        'X_U': X_U,                                                                                              
-        'PARAMETERS': PARAMETERS,                                                                                   
-        'NULL_DIC':{'RETURN': PORTIFOLIO_RETURNS, 'RISK': PORTIFOLIO_RISKS, 'RISK_FREE': RISK_FREE_ASSET, 'RP': 1E6}
-        } 
-        """)
+    
+        N_REP = st.number_input("N_REP", format="%0.1f")
+        N_POP = st.number_input("N_ITER", format="%0.1f")
+        N_ITER = st.number_input("N_POP", format="%0.1f")
 
         if st.button ("**Play**"):
-            st.markdown("Resultado")
+            FA_PORTIFOLIO.FIREFLY()
 
-    elif selected_instance == "Conjunto de ativos 2":
+    if selected_instance == "USA-3":
         st.markdown(
         """
-        | Empresa                  | Símbolo   |
-        |--------------------------|-----------|
-        |Engie Brasil Energia S.A. | EGIE3.SA  |
+        | Empresa                  | Símbolo   | Data Inicial | Data Final |
+        |--------------------------|-----------|--------------|------------|
+        |Engie Brasil Energia S.A. | EGIE3.SA  | xx/xx/xxxx   | xx/xx/xxxx |
         |Magazine Luiza S.A.       | MGLU3.SA  | 
         |Ambev S.A.                | ABEV3.SA  | 
         |Banco Bradesco S.A.       | BBDC3.SA  |
         |NIKE, Inc.                | NIKE34.SA |
         """
         )
-        stocks_set_2 = ["EGIE3.SA","NIKE34.SA", "ABEV3.SA","BBDC3.SA","MGLU3.SA"]
+        stocks_set_3 = ["EGIE3.SA","NIKE34.SA", "ABEV3.SA","BBDC3.SA","MGLU3.SA"]
 
-        start_day = datetime.date.today()
-        start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
-        end_day = date.today()
-        end_day = st.date_input("Selecione a data final",max_value=end_day)
+        #start_day = datetime.date.today()
+        #start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
+        #end_day = date.today()
+        #end_day = st.date_input("Selecione a data final",max_value=end_day)
         
-        if end_day < start_day:
-            st.error("A data final deve ser posterior à data inicial.")
+        #if end_day < start_day:
+        #    st.error("A data final deve ser posterior à data inicial.")
         
 #Setup e parametros
-        st.markdown("**Design space**")
-        st.markdown(
-        """
-        X_L = [0.00001] * D
-
-        X_U = [0.99999] * D
-        """)
-        st.markdown("**Setup FA method**")
-        st.markdown(
-        """" 
-        
-        GAMMA = GAMMA_ASSEMBLY(X_L, X_U, D, 2)
-        PARAMETERS = {
-                   'ATTRACTIVENESS (BETA_0)': 0.98,
-                   'MIN. RANDOM FACTOR (ALPHA_MIN)': 0.20,
-                   'MAX. RANDOM FACTOR (ALPHA_MAX)': 0.95,
-                   'LIGHT ABSORPTION (GAMMA)': GAMMA,
-                   'THETA': 0.98,
-                   'TYPE ALPHA UPDATE': 'YANG 0',
-                    'SCALING (S_D)': True
-        }""")
-        
-        st.markdown("**Setup optimization**")
-        st.markdown("{")
-        N_REP = st.number_input("N_REP", format="%0.0f")
-        N_POP = st.number_input("N_ITER", format="%0.0f")
-        N_ITER = st.number_input("N_POP", format="%0.0f")
-        st.markdown(
-        """
-        'D':   D,                                                                                                 
-        'X_L': X_L,                                                                                              
-        'X_U': X_U,                                                                                              
-        'PARAMETERS': PARAMETERS,                                                                                   
-        'NULL_DIC':{'RETURN': PORTIFOLIO_RETURNS, 'RISK': PORTIFOLIO_RISKS, 'RISK_FREE': RISK_FREE_ASSET, 'RP': 1E6}
-        } 
-        """)
+        N_REP = st.number_input("N_REP", format="%0.1f")
+        N_POP = st.number_input("N_ITER", format="%0.1f")
+        N_ITER = st.number_input("N_POP", format="%0.1f")
 
         if st.button ("**Play**"):
-            st.markdown("Resultado")
+            FA_PORTIFOLIO.FIREFLY()
 
 
-    if selected_instance == "Conjunto de ativos 3":
+    elif selected_instance == "USA-4":
         st.markdown(
         """
-        | Empresa                  | Símbolo   |
-        |--------------------------|-----------|
-        |C&A Modas S.A.            | CEAB3.SA  |
+        | Empresa                  | Símbolo   | Data Inicial | Data Final |
+        |--------------------------|-----------|--------------|------------|
+        |C&A Modas S.A.            | CEAB3.SA  | xx/xx/xxxx   | xx/xx/xxxx |
         |Itaúsa S.A.               | ITSA4.SA  | 
         |Atacadão S.A.             | CRFB3.SA  | 
         |Sony Group Corporation    | SNEC34.SA |
         |Nu Holdings Ltd.          | NUBR33.SA |
         """
         )
-        stocks_set_3 = ["NUBR33.SA","ITSA4.SA", "CEAB3.SA","SNEC34.SA","CRFB3.SA"]
+        stocks_set_4 = ["NUBR33.SA","ITSA4.SA", "CEAB3.SA","SNEC34.SA","CRFB3.SA"]
 
-        start_day = datetime.date.today()
-        start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
-        end_day = date.today()
-        end_day = st.date_input("Selecione a data final",max_value=end_day)
+        #start_day = datetime.date.today()
+        #start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
+        #end_day = date.today()
+        #end_day = st.date_input("Selecione a data final",max_value=end_day)
         
-        if end_day < start_day:
-            st.error("A data final deve ser posterior à data inicial.")
+        #if end_day < start_day:
+         #   st.error("A data final deve ser posterior à data inicial.")
         
 #Setup e parametros
-        st.markdown("**Design space**")
-        st.markdown(
-        """
-        X_L = [0.00001] * D
-
-        X_U = [0.99999] * D
-        """)
-        st.markdown("**Setup FA method**")
-        st.markdown(
-        """" 
         
-        GAMMA = GAMMA_ASSEMBLY(X_L, X_U, D, 2)
-        PARAMETERS = {
-                   'ATTRACTIVENESS (BETA_0)': 0.98,
-                   'MIN. RANDOM FACTOR (ALPHA_MIN)': 0.20,
-                   'MAX. RANDOM FACTOR (ALPHA_MAX)': 0.95,
-                   'LIGHT ABSORPTION (GAMMA)': GAMMA,
-                   'THETA': 0.98,
-                   'TYPE ALPHA UPDATE': 'YANG 0',
-                    'SCALING (S_D)': True
-        }""")
-        
-        st.markdown("**Setup optimization**")
-        st.markdown("{")
-        N_REP = st.number_input("N_REP", format="%0.0f")
-        N_POP = st.number_input("N_ITER", format="%0.0f")
-        N_ITER = st.number_input("N_POP", format="%0.0f")
-        st.markdown(
-        """
-        'D':   D,                                                                                                 
-        'X_L': X_L,                                                                                              
-        'X_U': X_U,                                                                                              
-        'PARAMETERS': PARAMETERS,                                                                                   
-        'NULL_DIC':{'RETURN': PORTIFOLIO_RETURNS, 'RISK': PORTIFOLIO_RISKS, 'RISK_FREE': RISK_FREE_ASSET, 'RP': 1E6}
-        } 
-        """)
+        N_REP = st.number_input("N_REP", format="%0.1f")
+        N_POP = st.number_input("N_ITER", format="%0.1f")
+        N_ITER = st.number_input("N_POP", format="%0.1f")
 
         if st.button ("**Play**"):
-            st.markdown("Resultado")
+            FA_PORTIFOLIO.FIREFLY()
 
 
-    elif selected_instance == "Custom":
-        #Conjunto de ativos
-        custom = st.text_input("**Digite os símbolos dos ativos:**")
-        stocks_custom = []
-        stocks_custom = custom.split(",")
-        stocks_custom = [stocks_custom.strip() for stocks_custom in stocks_custom]
+    # elif selected_instance == "Custom":
+    #     #Conjunto de ativos
+    #     custom = st.text_input("**Digite os símbolos dos ativos:**")
+    #     stocks_custom = []
+    #     stocks_custom = custom.split(",")
+    #     stocks_custom = [stocks_custom.strip() for stocks_custom in stocks_custom]
         
-        #Data
-        start_day = datetime.date.today()
-        start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
+    #     #Data
+    #     start_day = datetime.date.today()
+    #     start_day = st.date_input("**Selecione a data inicial**", max_value=start_day)
         
-        end_day = date.today()
-        end_day = st.date_input("Selecione a data final",max_value=end_day)
+    #     end_day = date.today()
+    #     end_day = st.date_input("Selecione a data final",max_value=end_day)
         
-        if end_day < start_day:
-            st.error("A data final deve ser posterior à data inicial.")
+    #     if end_day < start_day:
+    #         st.error("A data final deve ser posterior à data inicial.")
         
-        #Setup e parametros
-        st.markdown("**Design space**")
-        st.markdown(
-        """
-        X_L = [0.00001] * D
+    #     #Setup e parametros
+        
+    #     N_REP = st.number_input("N_REP", format="%0.0f")
+    #     N_POP = st.number_input("N_ITER", format="%0.0f")
+    #     N_ITER = st.number_input("N_POP", format="%0.0f")
 
-        X_U = [0.99999] * D
-        """)
-        st.markdown("**Setup FA method**")
-        st.markdown(
-        """" 
-        
-        GAMMA = GAMMA_ASSEMBLY(X_L, X_U, D, 2)
-        PARAMETERS = {
-                   'ATTRACTIVENESS (BETA_0)': 0.98,
-                   'MIN. RANDOM FACTOR (ALPHA_MIN)': 0.20,
-                   'MAX. RANDOM FACTOR (ALPHA_MAX)': 0.95,
-                   'LIGHT ABSORPTION (GAMMA)': GAMMA,
-                   'THETA': 0.98,
-                   'TYPE ALPHA UPDATE': 'YANG 0',
-                    'SCALING (S_D)': True
-        }""")
-        
-        st.markdown("**Setup optimization**")
-        st.markdown("{")
-        N_REP = st.number_input("N_REP", format="%0.0f")
-        N_POP = st.number_input("N_ITER", format="%0.0f")
-        N_ITER = st.number_input("N_POP", format="%0.0f")
-        st.markdown(
-        """
-        'D':   D,                                                                                                 
-        'X_L': X_L,                                                                                              
-        'X_U': X_U,                                                                                              
-        'PARAMETERS': PARAMETERS,                                                                                   
-        'NULL_DIC':{'RETURN': PORTIFOLIO_RETURNS, 'RISK': PORTIFOLIO_RISKS, 'RISK_FREE': RISK_FREE_ASSET, 'RP': 1E6}
-        } 
-        """)
-        if st.button ("**Play**"):
-            st.markdown("Resultado")
+    #     if st.button ("**Play**"):
+    #         FA_PORTIFOLIO.FIREFLY()
